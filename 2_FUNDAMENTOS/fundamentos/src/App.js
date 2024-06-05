ff09ec238534cd007a92cbb6e2d686d990ce5788
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-05 20:23:20 -0300

    Fundamentos React

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Inicio do Projeto */}
+      <h1>Fundamentos React</h1>
+      {/* Fim do Projeto */}
+    </div>
+  );
+}
+
+export default App;
