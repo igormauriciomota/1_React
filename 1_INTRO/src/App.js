7850dcf57d0c567215c3219bfc5d4a8367c9f954
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-05 14:46:30 -0300

    Atualização React

--- 1_INTRO/src/App.js
+++ 1_INTRO/src/App.js
@@ -3,9 +3,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Horld!</h1>
+      {/* Inicio do codigo wen */}
+
+      <section>
+                <div>
+                <h1 class="logo">Infor Tec</h1>
+                </div>
+                    <nav>
+                        <ul>
+                            <li class="primeiro">
+                                <a id="home" href="index.html">Home</a>
+                            </li>
+                            <li>
+                                <a id="linguagem" href="programe.html">Linguagem</a>
+                            </li>
+                            <li>
+                                <a id="banco" href="dados.html">Banco de Dados</a>
+                            </li>
+                            <li>
+                                <a id="tendencias" href="tende.html">Tendencias</a>
+                            </li>
+                            <li>
+                                <a id="seguranca" href="segu.html">Segurança</a>
+                            </li>
+                            <li>
+                                <a id="quiz" href="qui.html">Quiz</a>
+                            </li>
+                        </ul>
+                    </nav>
+            </section>
+      
+      {/* Fim do codigo web */}
     </div>
   );
 }
 
 export default App;
+
