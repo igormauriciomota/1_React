388165399853ceae4455ce5545a3f8b4d7ad0044
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-06 20:45:47 -0300

    Projeto avanção de React

---
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Inicio Projeto */}
+      <div>
+        <h1>Avançando em React</h1>
+
+        {/* Imagem em public */}
+        <div>
+          <img src="/Genesis.png" alt="Genesis" />
+        </div>
+      </div>
+      {/* Fim do Projeto */}
+    </div>
+  );
+}
+
+export default App;
