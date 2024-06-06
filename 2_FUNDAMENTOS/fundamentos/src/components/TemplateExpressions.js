02dcd0953ed38b9d8e30200de7ef30c53dbe4e9f
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-06 10:32:56 -0300

    Criação do segundo elemento TemplateExpressions

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.js
@@ -0,0 +1,12 @@
+const TemplateExpressions = () => {
+    const name = "Igor Mota";
+
+    return (
+        <div>
+            <h1>Olá {name}, tudo bem?</h1>
+        </div>
+    );
+
+};
+
+export default TemplateExpressions;
