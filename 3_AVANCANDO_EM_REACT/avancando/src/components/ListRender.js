e462585027eef5d96596c7d72392f6d71d440f67
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-07 19:37:07 -0300

    liste render

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
@@ -3,13 +3,40 @@ import { useState } from 'react';
 const ListRender = () => {
     const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
 
+// Função
+    const [users, setUsers] = useState([
+        {id: 1, name: "Maheus", age: 31 },
+        {id: 2, name: "João", age: 28 },
+        {id: 3, name: "Pedro", age: 44 },
+    ]);
+
+// Função Delete pegar o id: de 1 a 3;
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            console.log(prevUsers);
+            return prevUsers.filter((user) => randomNumber !== user.id)
+
+        })
+    };
+
+
     return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
             ))}
         </ul>
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>
+                    {user.name} - {user.age}
+                </li>
+            ))}
+        </ul>
+        <button onClick={deleteRandom}>Delete random user</button>
     </div>
     )
 };
