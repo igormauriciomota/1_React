7595181d9d086f3d15c6c575141a48e54ee23d6f
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-11 19:14:55 -0300

    Form com React

---
+++ 5_FORM_EM_REACT/challeng/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import MyForm from './components/MyForm';
+
+function App() {
+  return (
+    <div className="App">
+    {/* Inicio Formulario */}
+      <h2>Formulario</h2>
+      <MyForm />
+
+
+    {/* Fim form */}
+    </div>
+  );
+}
+
+export default App;
