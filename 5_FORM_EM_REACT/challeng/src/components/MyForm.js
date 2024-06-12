a748ffa085ccee6908b4888ba9f698ef83e1a6d7
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-12 17:13:24 -0300

    OnChange: acontece quando uma tecla e pressionada no imput

--- 5_FORM_EM_REACT/challeng/src/components/MyForm.js
+++ 5_FORM_EM_REACT/challeng/src/components/MyForm.js
@@ -1,13 +1,27 @@
 import './MyForm.css';
 
+import { useState } from 'react';
+
 const MyForm = () => {
+  // 3 - gerenciamento de dados
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+
+  // teremos uma função referente a gerenciamento de dados 3
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  console.log(name);
+
+
   return (
     <div>
         {/* 1 - Criação de Form */}
         <form>
             <div>
             <label htmlFor="name">Nome:</label>
-            <input type="text" name="name" placeholder="Digite o seu nome"/>
+            <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName}/>
             </div>
             {/* 2 - label envolvendo input */}
             <label>
